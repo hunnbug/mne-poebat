89c9b4aaf92861d168cf6edc5909e4774e54c394
hunnbug <feldolshkolnik@gmail.com>
2024-05-31 21:39:07 +0300

    Asda

--- tests/p6test.py
+++ tests/p6test.py
@@ -5,8 +5,8 @@ class TestsOfWordsCounter(unittest.TestCase): # создаем класс тес
     def test_otric_len(self):
         self.assertEqual(ploshad(-1, 2), "длина не может быть отрицательной") # проверка на отрицательность длины
     def test_null_len(self):
-        self.assertEqual(ploshad(-1, 2), "длина не может равняться нулю") # проверка на то, является ли длина нулем
+        self.assertEqual(ploshad(0, 2), "длина не может равняться нулю") # проверка на то, является ли длина нулем
     def test_otric_wid(self):
-        self.assertEqual(ploshad(2, -1), "длина не может быть отрицательной") # проверка на отрицательность ширины
+        self.assertEqual(ploshad(2, -1), "ширина не может быть отрицательной") # проверка на отрицательность ширины
     def test_null_wid(self):
-        self.assertEqual(ploshad(2, -1), "длина не может равняться нулю") # проверка на то, является ли ширина нулем
+        self.assertEqual(ploshad(2, 0), "ширина не может равняться нулю") # проверка на то, является ли ширина нулем
